be44ca1395e37f2e989623bd1421ad778d592fdb
olesjadon <doledile@ukr.net>
2022-10-11 15:06:19 +0000

    my project

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,31 +1,20 @@
-function openModal() {
-document.getElementById('myModal').style.display = "block";
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-  }
-  
-  let slideIndex = 1;
-  showSlides(slideIndex);
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  function correntSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  function showSlides(n) {
-    let i;
-    const slides = document.getElementsByClassName('mySlides');
-    let dots = document.getElementsByClassName('demo');
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i=0; i < slides.length; i++){
-slides[i].style.display = "none";}
-    for (i = 0; i < dots.length; i++) {
-dots[i].className = dots[i].className.replace(" active", "");
-    }
-   slides[slideIndex-1].style.display = 'block';
-    dots[slideIndex-11].className += ' active';
-  }
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}

2487e388c03f5a0848ab45c1f98f661d374d45cd
olesjadon <doledile@ukr.net>
2022-10-11 12:55:35 +0000

    js new

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,43 +1,31 @@
-const slideIndex = 1;
-showSlide(slideIndex);
-
-function openLightbox() {
-  document.getElementById('Lightbox').style.display = 'block';
+function openModal() {
+document.getElementById('myModal').style.display = "block";
 }
-
-function closeLightbox() {
-  document.getElementById('Lightbox').style.display = 'none';
-}
-
-function changeSlide(n) {
-  showSlide(slideIndex += n);
-}
-
-function toSlide(n) {
-  showSlide(slideIndex = n);
-}
-
-function showSlide(n) {
-
-  const slides = document.getElementsByClassName('slide');
-  let modalPreviews = document.getElementsByClassName('modal-preview');
-
-  if (n > slides.length) {
-    slideIndex = 1; 
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
   }
   
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+  let slideIndex = 1;
+  showSlides(slideIndex);
 
-  for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
   }
-  for (let i = 0; i < modalPreviews.length; i++) {
-      modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
+  function correntSlide(n) {
+    showSlides(slideIndex = n);
   }
-  
-  slides[slideIndex - 1].style.display = 'block';
-  modalPreviews[slideIndex - 1].className += ' active';
-}
+  function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName('mySlides');
+    let dots = document.getElementsByClassName('demo');
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i=0; i < slides.length; i++){
+slides[i].style.display = "none";}
+    for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace(" active", "");
+    }
+   slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-11].className += ' active';
+  }
 
